88d42905c66c4aad04d4802dd987ac9eccd20017
muflihtsani9 <muflihtsani9@gmail.com>
2026-06-17 19:47:11 +0700

    update

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Perfect',
-    artist:    'Ed Sheeran',
+    title:     'Sampai Jadi Debu',
+    artist:    'Banda Neira',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
